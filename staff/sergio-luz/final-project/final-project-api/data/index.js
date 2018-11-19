c407cf23c127c0c15f72af1f830f9ef317eefb76
Sergio <sergio.luz.fernandez@gmail.com>
2018-11-19 10:16:05 +0100

    initial commit

---
+++ staff/sergio-luz/final-project/final-project-api/data/index.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+const { Postit, User } = require('./schemas')
+
+module.exports = {
+    User: mongoose.model('User', User),
+    Postit: mongoose.model('Postit', Postit)
+}
